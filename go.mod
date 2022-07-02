da7614bd1e395007e02064caa468c102ee4fb61e
chanwit-y <chanwit.yimneam@hotmail.com>
2022-07-02 07:47:10 +0700

    init

---
+++ go.mod
@@ -0,0 +1,8 @@
+module go-read-qr-img-pdf
+
+go 1.18
+
+require (
+	github.com/karmdip-mi/go-fitz v0.0.0-20210702102225-a530a79566e9
+	github.com/liyue201/goqr v0.0.0-20200803022322-df443203d4ea
+)
---
+++ main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"image"
+	"image/jpeg"
+
+	// _ "image/jpeg"
+	_ "image/png"
+	"io/ioutil"
+	"os"
+	"path"
+	"path/filepath"
+	"strings"
+
+	"github.com/liyue201/goqr"
+
+	"github.com/karmdip-mi/go-fitz"
+)
+
+func convertPdf2Image() string {
+	var fileName string
+	files := []string{"qr.pdf"}
+
+	for _, file := range files {
+		doc, err := fitz.New(file)
+		if err != nil {
+			panic(err)
+		}
+		folder := strings.TrimSuffix(path.Base(file), filepath.Ext(path.Base(file)))
+
+		// Extract pages as images
+		for n := 0; n < doc.NumPage(); n++ {
+			img, err := doc.Image(n)
+			if err != nil {
+				panic(err)
+			}
+			err = os.MkdirAll("img/"+folder, 0755)
+			if err != nil {
+				panic(err)
+			}
+
+			fileName = fmt.Sprintf("image-%05d.jpg", n)
+			f, err := os.Create(filepath.Join("img/"+folder+"/", fileName))
+			if err != nil {
+				panic(err)
+			}
+
+			err = jpeg.Encode(f, img, &jpeg.Options{Quality: jpeg.DefaultQuality})
+			if err != nil {
+				panic(err)
+			}
+
+			f.Close()
+
+		}
+	}
+
+	return fileName
+}
+
+func readImageQrCode(path string) {
+	fmt.Printf("recognize file: %v\n", path)
+	imgdata, err := ioutil.ReadFile(path)
+	if err != nil {
+		fmt.Printf("%v\n", err)
+		return
+	}
+	img, _, err := image.Decode(bytes.NewReader(imgdata))
+	if err != nil {
+		fmt.Printf("image.Decode error: %v\n", err)
+		return
+	}
+	qrCodes, err := goqr.Recognize(img)
+	if err != nil {
+		fmt.Printf("Recognize failed: %v\n", err)
+		return
+	}
+	for _, qrCode := range qrCodes {
+		fmt.Printf("qrCode text: %s\n", qrCode.Payload)
+	}
+	return
+}
+
+func main() {
+	readImageQrCode("qr.png")
+	// readImageQrCode(fmt.Sprintf("img/qr/%s", convertPdf2Image()))
+}
